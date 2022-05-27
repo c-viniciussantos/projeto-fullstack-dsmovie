e1c615fe2f8605b550eec066372c535700110edd
Vinicius Santos <c.viniciussantos@outlook.com>
2022-05-26 22:36:55 -0300

    Project Created

---
+++ backend/src/main/java/com/cviniciussantos/projetofullstackdsmovie/ProjetoFullstackDsmovieApplication.java
@@ -0,0 +1,13 @@
+package com.cviniciussantos.projetofullstackdsmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoFullstackDsmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoFullstackDsmovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cviniciussantos/projetofullstackdsmovie/ProjetoFullstackDsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cviniciussantos.projetofullstackdsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoFullstackDsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
